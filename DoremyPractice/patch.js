b1466fca3c40cffcd229dcb284b072f637f326de
rosepenguin <roseporcupine@gmail.com>
2024-11-04 10:50:09 -0600

    Update patch.js

--- DoremyPractice/patch.js
+++ DoremyPractice/patch.js
@@ -3,7 +3,7 @@
 	"servers": [
 		"https://mirrors.thpatch.net/RosePenguin/DoremyPractice/"
 	],
-	"title": "(TH15) Limits even waves of Doremy's lunatic opener to be within a specific range of angles. By renaming files in the patch folder, you can select which set of angles you want to practice. The files included cover all possible cases for Doremy non 1."
+	"title": "(TH15) Limits even waves of Doremy's lunatic opener to be within a specific range of angles. By renaming files in the patch folder, you can select which set of angles you want to practice. The files included cover all possible cases for Doremy non 1.",
 	"dependencies": [
 		"nmlgc/base_tsa"
 	],
