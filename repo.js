47dc760a7a2a0398c5b197f88c13989d79c2c56c
rosepenguin <roseporcupine@gmail.com>
2024-11-04 09:29:24 -0600

    Add files via upload

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "@rosepenguin100",
+    "id": "RosePenguin",
+    "patches": {
+        "Th15DoremyPractice": "Limits even waves of Doremy's lunatic opener to be within a specific range of angles. By renaming files in the patch folder, you can select which set of angles you want to practice. The files included cover all possible cases for Doremy non 1."
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/RosePenguin/"
+    ],
+    "title": "RosePenguin's Repository"
+}
